7e87e154a81c8feeb9d21688c57f4c6389664498
kris <kris@xoofoo.org>
2013-07-09 01:08:48 +0200

    Commit based on xoofoo sourceforge repository

---
+++ language/english/script.js
@@ -0,0 +1,2 @@
+/* $Id: script.js 135 2010-02-16 17:29:49Z kris_fr $ */
+/* Localization script */
---
+++ language/french_iso/script.js
@@ -0,0 +1,2 @@
+/* $Id: script.js 224 2010-04-02 12:18:00Z kris_fr $ */
+/* Localization script */
